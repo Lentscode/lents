a1f9d345c53d2652af6d9f6a2d91f324acdcd5ff
Lentscode <antonio.lentini2004@gmail.com>
2024-04-28 21:59:23 +0200

    Espansa documentazione

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
 # Lents package for Dart/Flutter
 
 Package that provides a lot of useful ready-to-use widgets and classes for Dart/Flutter projects.
 
-It is built upon the Dart and Flutter library and other packages like ```cloud_firestore``` for Firestore and ```algolia_flutter_helper``` for Algolia.
+It is built upon the Dart and Flutter library and other packages like `cloud_firestore` for Firestore and `algolia_flutter_helper` for Algolia.
 It simply provides a faster and simple way to do multiple things common in a project, like fetching data from Algolia or Firestore, or displaying the profile image of a user.
--- lib/src/firestore/paginated_firestore_query_cubit.dart
+++ lib/src/firestore/paginated_firestore_query_cubit.dart
@@ -2,19 +2,51 @@ import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
+/// A cubit that handles paginated queries to Firestore.
+///
+/// This cubit is responsible for fetching documents from Firestore, thanks to [queryBuilder],
+/// and converting them to a list of items of type `T`, using [fromSnapshot].
+///
+/// The [docsPerPage] parameter defines how many documents will be fetched at once.
+/// The [queryBuilder] function should return a [Query] object that will be used to fetch the documents.
+///
+/// The cubit has two main methods: [loadData] and [loadMore].
+///
+/// * [loadData] fetches the first batch of documents.
+/// * [loadMore] fetches the next batch of documents.
+///
+/// **RECOMMENDATION**
+///
+/// This cubit is designed to be used with the `FirestoreSliverList` widget.
+/// Indeed, `FirestoreSliverList` is a widget that uses this cubit to fetch documents from Firestore and
+/// display them in a paginated way.
+///
+/// _P.S._
+///
+/// (We opted for a [Cubit] instead of a [Bloc] because it makes the process easier.)
 class PaginatedFirestoreQueryCubit<T> extends Cubit<PaginatedFirestoreQueryState<T>> {
+  /// Creates a new [PaginatedFirestoreQueryCubit] instance.
+  ///
+  /// It automatically emits a [PaginatedFirestoreQueryState] with an empty list of data and [hasMoreData] set to `true`.
   PaginatedFirestoreQueryCubit({
     required this.queryBuilder,
     required this.fromSnapshot,
     this.docsPerPage = 10,
   }) : super(PaginatedFirestoreQueryState<T>(data: const [], hasMoreData: true));
 
+  /// The function that builds the query to fetch the documents.
   final Query Function() queryBuilder;
+
+  /// The function that converts a [DocumentSnapshot] to an item of type `T`.
   final T Function(DocumentSnapshot data) fromSnapshot;
+
+  /// The number of documents to fetch at once.
   final int docsPerPage;
 
+  /// The last document fetched, used in [loadMore] to set the new starting point of the query.
   DocumentSnapshot? _lastDocument;
 
+  /// Fetches the first batch of documents.
   Future<void> loadData() async {
     Query query = queryBuilder().limit(docsPerPage);
 
@@ -22,6 +54,7 @@ class PaginatedFirestoreQueryCubit<T> extends Cubit<PaginatedFirestoreQueryState
     onDataFetched(snapshot);
   }
 
+  /// Fetches the next batch of documents.
   Future<void> loadMore() async {
     if (_lastDocument == null) return;
 
@@ -31,6 +64,7 @@ class PaginatedFirestoreQueryCubit<T> extends Cubit<PaginatedFirestoreQueryState
     onDataFetched(snapshot);
   }
 
+  /// Updates the state with the new data fetched.
   void onDataFetched(QuerySnapshot snapshot) {
     final items = List<T>.from(state.data)..addAll(snapshot.docs.map(fromSnapshot).toList());
     _lastDocument = snapshot.docs.isNotEmpty ? snapshot.docs.last : null;
@@ -40,6 +74,7 @@ class PaginatedFirestoreQueryCubit<T> extends Cubit<PaginatedFirestoreQueryState
     emit(state.copyWith(data: items, hasMoreData: hasMoreData));
   }
 }
+
 
 class PaginatedFirestoreQueryState<T> extends Equatable {
   final List<T> data;
